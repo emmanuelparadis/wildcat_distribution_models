9531cbd310277387e45ff4424329254a2494020e
Emmanuel Paradis <emmanuelparadis@users.noreply.github.com>
2026-09-07 14:58:40 +0200

    Update script_prepare_GADM_data.R

--- script_prepare_GADM_data.R
+++ script_prepare_GADM_data.R
@@ -6,7 +6,7 @@ ex <- c(xmin = -4.8194444444305, xmax = 8.25000000001498,
         ymin = 42.3194444444406,  ymax = 51.111111111108)
 
 ## mask with France's borders
-od <- setwd("/media/paradis/12674652-3312-4cf3-9e96-3b970feca8c6/GADM")
+od <- setwd("..................") # gie the path to the directory where the file below is stored
 st_layers("gadm_410-levels.gpkg")
 GADM <- read_sf("gadm_410-levels.gpkg", "ADM_0", as_tibble = FALSE)
 setwd(od)
